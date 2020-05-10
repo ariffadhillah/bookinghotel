7907f74fc5da27e32c44b82937acd2052fce83b1
ariffadhillah <ariffadhillah319@gmail.com>
2020-05-10 12:05:40 -0700

    btn component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "assets/scss/style.scss";
 
 function App() {
   return (
+
     <div className="App">
-    hai
+      hai dunia
     </div>
-  );
+    );
 }
 
 export default App;
