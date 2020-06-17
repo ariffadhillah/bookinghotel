90fb4d5617080c7d145e04b537c069f664a84e3b
ariffadhillah <ariffadhillah319@gmail.com>
2020-06-17 08:26:07 -0700

    card error sucses

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Button from "elements/Button";
+import Button from "elements/Button";
 
 export default function MostPicked(props) {
   
@@ -10,9 +10,28 @@ export default function MostPicked(props) {
           {props.data.map((item, index) => {
               return  (
                 <div 
-                  className={`item column-4${index === 0 ? "row-2" : " row-1" }`}
+                  className={`item column-4${index === 0 ? " row-2" : " row-1" }`}
                 >
-                  <div className="card">Card{index = 1}</div>
+                  <div className="card card-featured">
+                      <div className="tag">
+                        ${item.price}
+                        <span className="font-weight-light">per { item.unit}</span>
+                      </div>
+                      <figure className="img-wrapper">
+                        <img 
+                          src={item.imageUrl} 
+                          alt={item.name}
+                          className="img-cover"
+                          />
+                      </figure>
+                        <Button type="link" className="stretched-link d-block text-white" href={`/properties/${item._id}`}>
+                          <h5>{item.name}</h5>
+                        </Button>
+                          <span>
+                            {item.city}, {item.country}
+                          </span>
+
+                  </div>
                 </div>
               );
             })}
