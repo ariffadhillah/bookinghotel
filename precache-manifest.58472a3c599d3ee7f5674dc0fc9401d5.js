3d36de9553898b7aea317026961bfaea472e1cd6
ariffadhillah <ariffadhillah319@gmail.com>
2020-06-28 11:18:26 -0700

    Updates

---
+++ precache-manifest.58472a3c599d3ee7f5674dc0fc9401d5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42510691e99c51eaf9cbcdf7ae6d6c3f",
+    "url": "/bookinghotel/index.html"
+  },
+  {
+    "revision": "bbd105150acd5bbabc38",
+    "url": "/bookinghotel/static/css/main.f5179d2c.chunk.css"
+  },
+  {
+    "revision": "0b369c2ee6928154e65a",
+    "url": "/bookinghotel/static/js/2.e73e3a24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bookinghotel/static/js/2.e73e3a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd105150acd5bbabc38",
+    "url": "/bookinghotel/static/js/main.c0cfe965.chunk.js"
+  },
+  {
+    "revision": "9ee861e14c24e3e66bf6",
+    "url": "/bookinghotel/static/js/runtime-main.bd24062f.js"
+  },
+  {
+    "revision": "bc95ea7081dab6207da118aa85e6c3fa",
+    "url": "/bookinghotel/static/media/icon-cities.bc95ea70.svg"
+  },
+  {
+    "revision": "109e18e8160fb2c8887bfc75e9cbc2c6",
+    "url": "/bookinghotel/static/media/icon-traveler.109e18e8.svg"
+  },
+  {
+    "revision": "bb259d86e7904d0ce5931ebee9924b69",
+    "url": "/bookinghotel/static/media/icon-treasure.bb259d86.svg"
+  },
+  {
+    "revision": "19fa22a679fb41aa197b518e0eb2d802",
+    "url": "/bookinghotel/static/media/img-hero-frame.19fa22a6.jpg"
+  },
+  {
+    "revision": "97bfd6d983540d6604427f3bf547c0a5",
+    "url": "/bookinghotel/static/media/img-hero.97bfd6d9.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/bookinghotel/static/media/star.11a62c71.svg"
+  },
+  {
+    "revision": "1632c742f5db38b80e51f3bce1b16371",
+    "url": "/bookinghotel/static/media/testimonial-landingpages-frame.1632c742.jpg"
+  }
+]);
