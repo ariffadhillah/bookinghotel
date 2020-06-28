b11d4bcac6692a14719d6b84a3b8f8fddcf3bba0
ariffadhillah <ariffadhillah319@gmail.com>
2020-06-28 10:06:43 -0700

    landing pages succes

---
+++ src/parts/Footer.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import Button from 'elements/Button';
+import IconText from 'parts/IconText';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-auto" style={{width: 350}}>
+                        <IconText />
+                        <p className="brand-tagline">
+                            We kaboom your beauty holiday instantly and memorable.
+                        </p>
+                    </div>
+                    <div className="col-2 mr-5">
+                        <h6 className="mt-2">For Beinners</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/register">New Account</Button>
+                            </li>
+                        </ul>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/properties">properties</Button>
+                            </li>
+                        </ul>                        
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/use-payments">Use Payments</Button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-2 mr-5">
+                        <h6 className="mt-2">Explore Us</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/Careers">Our Careers</Button>
+                            </li>
+                        </ul>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/privacy">Privacy</Button>
+                            </li>
+                        </ul>                        
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/terms">Terms & Conditions</Button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <h6 className="mt-2">Connect Us</h6>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button isExternal type="link" href="mailto:support@staycation.id">support@staycation.id</Button>
+                            </li>
+                        </ul>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button isExternal type="link" href="tel:+622122081996">021 - 2208 - 1996</Button>
+                            </li>
+                        </ul>                        
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <Button type="link" href="/terms">Terms & Conditions</Button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col text-center copyrights">
+                        Copyright 2019 - All reserves - Staycation
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
